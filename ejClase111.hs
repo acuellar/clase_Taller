b3d3adf6a50b25473312fad98f9d5ed857bbe7aa
ariel <ariel@Thor.(none)>
2014-11-26 06:15:28 -0300

    polinomios

--- ejClase111.hs
+++ ejClase111.hs
@@ -2,46 +2,20 @@ data Polinomio = Mono Float Integer
                | Suma Polinomio Polinomio
                | Producto Polinomio Polinomio deriving Eq
 instance Show Polinomio where
-{-  show (Mono a n) | n == 0 = show a
-                  | n == 1 = show a ++ " x"
-                  | a >= 0 =  show a ++ " x^" ++ show n
-                  | a <  0 = " - " ++ show (Mono (abs a) n)
-  -}
-
-  show (Mono 0 n) = "0"
+  show (Mono 0 n) = " 0"
   show (Mono a 0) = show a
   show (Mono a 1) = show a ++ " x"
   show (Mono a n) = show a ++ " x^" ++ show n
 --  show (Suma p (Mono a n)) = show' (Suma p (Mono a n))
 
   show (Producto p q) = "(" ++ show p ++ ") * (" ++ show q ++ ")"
---  show (Suma  p (Mono a n)) =  show (Suma (Mono a n) p)
-{-
-  show (Suma (Mono a n) p)
-        | a <  0 = show p ++ " - " ++ show (Mono (abs a) n) 
-        | a >  0 = show p ++ " + " ++ show (Mono a n)
---        | a == 0 = show p
-        | otherwise = show p
--}        
+
   show (Suma p (Mono a n))
         | a <  0 = show p ++ " - " ++ show (Mono (abs a) n) 
         | a >  0 = show p ++ " + " ++ show (Mono a n)
 --        | a == 0 = show p
         | otherwise = show p
---  show (Suma p q ) = show p ++ " + " ++ show q
-{-
-show' (Suma (Mono a n) (Mono b m))
-        | a < 0 && b > 0 = " - " ++ show (Mono (abs a) n) ++ " + " ++ show (Mono b m)
-        | a > 0 && b < 0 = show (Mono a n) ++ " - " ++ show (Mono (abs b) m)
-        | a < 0 && b < 0 = " - " ++ show (Mono (abs a) n) ++ " - " ++ show (Mono (abs b) m)
-        | a == 0 = show (Mono b m) 
-        | b == 0 = show (Mono a n)
-        | a == 0 && b == 0 = show (0)
-        | otherwise = show (Mono a n) ++ " + " ++ show (Mono b m)
 
-showSum (Mono a n) | a < 0 = " - " ++ (show (abs a)) ++ " x^" ++ (show n)
-                   | otherwise = show (Mono a n)
--}
 instance Num Polinomio where
   p + q = Suma p q
   p * q = Producto p q
@@ -62,14 +36,18 @@ coefs (Suma p q) = sumaListas (coefs p) (coefs q)
 coefs (Producto p q) = productoListas (coefs p) (coefs q)
 
 polinomioSuma :: Polinomio -> Polinomio -> Polinomio
-polinomioSuma p q = sumaMono (reverse(coefs (Suma p q))) (toInteger(length(coefs (Suma p q)))-1)
+polinomioSuma s1 s2 = polinomioDe (eliminarCeros(coefs (Suma s1 s2))) 
+
+polinomioDe :: [Float] -> Polinomio
+polinomioDe [] = Mono 0 0
+polinomioDe (x:xs) = sumaMono (reverse(x:xs)) (toInteger(length(x:xs))-1)
 
 sumaMono :: [Float] -> Integer -> Polinomio
 sumaMono (x:xs) n |    n == 0 = (Mono x 0)
                   | otherwise = (sumaMono xs (n-1)) + (Mono x n)
 
 polinomioProducto :: Polinomio -> Polinomio -> Polinomio
-polinomioProducto p q = sumaMono (reverse(coefs (Producto p q))) (toInteger(length(coefs (Producto p q)))-1)
+polinomioProducto h1 h2 = polinomioDe (eliminarCeros(coefs (Producto h1 h2)))
 
 sumaListas :: [Float] -> [Float] -> [Float]
 sumaListas [] ys = ys
@@ -104,16 +82,6 @@ gradoPol p = toInteger(length (eliminarCeros (coefs p)) - 1)
 coefPpal :: Polinomio -> Float
 coefPpal p = last(coefs p)
 
-divP :: Polinomio -> Polinomio -> Polinomio
-divP p q = sumaMono (reverse(divPol p q)) (toInteger(length(divPol p q))-1)
-
-divPol p q = divPoli (coefs p) (coefPpal p) (gradoPol p) (coefs q) (coefPpal q) (gradoPol q)
-
-divPoli lp cpp gp lq cpq gq
-  |gp < gq = []
-  |otherwise = (divPoli  (restaLista lp cpp gp lq cpq gq) (last (restaLista lp cpp gp lq cpq gq)) (toInteger (length (restaLista lp cpp gp lq cpq gq)-1)) lq cpq gq ) ++ [(cpp / cpq)]  
-
-restaLista lp cpp gp lq cpq gq = eliminarCeros (sumaListas lp (cteLista ((-1)*(cpp / cpq)) ((construirListaDe (gp - gq) 0) ++ lq )))
 
 construirListaDe :: Integer -> Float -> [Float]
 construirListaDe cont valor
@@ -121,44 +89,64 @@ construirListaDe cont valor
                | otherwise  = valor : construirListaDe (cont - 1) valor
 
 
+       
+
+--divP :: Polinomio -> Polinomio -> Polinomio
+divP p1 p2 
+  | gradoPol p2 < 0 = error "no es un numero, o infinito"
+  | gradoPol p1 < gradoPol p2 = Mono 0 0
+  | otherwise = polinomioDe(divPol p1 p2)
+
+divPol :: Polinomio -> Polinomio -> [Float]
+divPol p1 p2 = divPoli (reverse(coefs p1)) (reverse(coefs p2) ++ (construirListaDe difg 0)) (difg + 1)  
+            where difg =  toInteger(length(coefs p1) - length(coefs p2))
+
+divPoli :: [Float] -> [Float] -> Integer -> [Float]
+divPoli (x:xs) (y:ys) n 
+  | n==0 = []
+  |otherwise = (divPoli (tail(restoParcial) ++ [0]) (y:ys) (n-1) ) ++ [ x/y ]  
+                  where restoParcial = (restaLista (x:xs) (y:ys)) 
+       
+restaLista (x:xs) (y:ys) = sumaListas (x:xs) (cteLista ((-1)*(x/y)) (y:ys))
+
 modP :: Polinomio -> Polinomio -> Polinomio
-modP p q = polinomioSuma p (opuesto (polinomioProducto (divP p q) q))
+modP q1 q2 
+  | gradoPol q1 < gradoPol q2 = q1 
+  | otherwise = polinomioSuma q1 (opuesto (polinomioProducto (divP q1 q2) q2))
+
 
 
 euclidesP :: Polinomio -> Polinomio -> Polinomio
 euclidesP q r 
 --  | coefs r == (Mono 0 0) = q
-  |  gradoPol r == (-1)  = q
+  |  gradoPol r <= 0  = q
   | otherwise = euclidesP  r (modP q r) 
 
-{-  
-euclidesP :: Polinomio -> Polinomio -> (Polinomio, Polinomio, Polinomio)
-euclidesP 0 b = (b, 0, 1)
-euclidesP a b = (d, m - (b `divP` a) * n, n)
-  where
-     (d, n, m) = euclidesP (b `modP` a) a
 
+{-  
+*Main> let p = ( Mono 1 1) + ( Mono ( 2) 3) + ( Mono (2) 5)
 *Main> let q = ( Mono 2 0) + ( Mono ( -3) 1) + ( Mono 1 2)
-*Main> let p = Suma ( Mono 1 0) ( Mono 2 1)
-*Main> q
--3.0 x + 2.0 + 1.0 x^2
-*Main> p
-2.0 x + 1.0
-*Main> polinomioProducto p q
-2.0 + 1.0 x - 5.0 x^2 + 2.0 x^3
-*Main> polinomioSuma p q
-3.0 - 1.0 x + 1.0 x^2
-*Main> p + q
-2.0 x + 1.0 + -3.0 x + 2.0 + 1.0 x^2
-*Main> let q = ( Mono (-1) 0) + ( Mono ( 1) 4)
-*Main> let p = ( Mono 1 1) + ( Mono ( 2) 3) + ( Mono (1) 5)
 *Main> divP p q
-0 + 1.0 x
-*Main> p
-1.0 x + 2.0 x^3 + 1.0 x^5
-*Main> q
--1.0 + 1.0 x^4
+36.0 + 16.0 x + 6.0 x^2 + 2.0 x^3
 *Main> modP p q
-0 + 2.0 x + 2.0 x^3
-*Main> euclidesP p q
+-72.0 + 77.0 x
+*Main> let h = modP p q
+*Main> h
+-72.0 + 77.0 x
+*Main> divP q h
+^CInterrupted.
+*Main> q
+2.0 - 3.0 x + 1.0 x^2
+*Main> h
+-72.0 + 77.0 x
+*Main> divP q h
+^CInterrupted.
+*Main> let h = ( Mono (-72) 0) + ( Mono 77 1) 
+*Main> h
+-72.0 + 77.0 x
+*Main> divP q h
+-2.681734e-2 + 1.2987013e-2 x
+
+
+
 -}
